b577c562a3c224dd3d4c0367c76331367ef4774c
Andrew Clavin <andrewclavin8@gmail.com>
2018-04-24 14:21:56 -0400

    curl functionality in bash

--- bash.js
+++ bash.js
@@ -1,13 +1,16 @@
 var pwd = require('./pwd.js');
 var ls = require('./ls.js');
 var cat = require('./cat.js');
+var curl = require('./curl.js');
 process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   if(cmd === 'pwd') {
     pwd();
-} else if(cmd.substr(0,3) === 'cat') {
+  } else if(cmd.substr(0, 3) === 'cat') {
     cat(cmd.substr(4));
+  } else if(cmd.substr(0, 4) === 'curl') {
+    curl(cmd.substr(4));
   } else if(cmd === 'ls') {
     ls();
   } else {
